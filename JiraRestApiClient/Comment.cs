11ca025f2de34325d36e957c48d4aac834f3473f
vma <vma@compent.net>
2016-07-27 22:26:56 +0200

    second

---
+++ JiraRestApiClient/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TechTalk.JiraRestClient
+{
+    public class Comment
+    {
+        public string id { get; set; }
+        public string body { get; set; }
+    }
+}
---
+++ JiraRestApiClient/IssueFields.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechTalk.JiraRestClient
+{
+    public class IssueFields
+    {
+        public IssueFields()
+        {
+            status = new Status();
+            timetracking = new Timetracking();
+
+            labels = new List<String>();
+            comments = new List<Comment>();
+            issuelinks = new List<IssueLink>();
+            attachment = new List<Attachment>();
+            watchers = new List<JiraUser>();
+        }
+
+        public String summary { get; set; }
+        public String description { get; set; }
+        public Timetracking timetracking { get; set; }
+        public Status status { get; set; }
+
+        public JiraUser reporter { get; set; }
+        public JiraUser assignee { get; set; }
+        public List<JiraUser> watchers { get; set; } 
+
+        public List<String> labels { get; set; }
+        public List<Comment> comments { get; set; }
+        public List<IssueLink> issuelinks { get; set; }
+        public List<Attachment> attachment { get; set; }
+    }
+}
