2e3edfd2811f1ae4f084fcf8674c767c802c98c7
vma <vma@compent.net>
2016-08-01 23:05:48 +0200

    add mvc client

---
+++ WebClient/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It&rsquo;s time for wassup bro ;)";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("TestView");
+        }
+    }
+}
